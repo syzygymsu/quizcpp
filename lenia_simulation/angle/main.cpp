227ccf55b2a3661438b981e56541dc29b8936d3d
a.savanovich <asavanster@gmail.com>
2020-12-24 15:32:59 +0300

    add tests

--- lenia_simulation/angle/main.cpp
+++ lenia_simulation/angle/main.cpp
@@ -1,5 +1,7 @@
 #include <cmath>
 #include <algorithm>
+#include <iostream>
+
 class Device {
 public:
     Device(double acc, double dec, double top)
@@ -22,29 +24,86 @@ private:
 
 double Device::time(double angle) const {
     double critical = 2 * angle * acc * dec / (acc + dec);
-    if (critical <= top * top) {
+    if (critical < top * top) {
         return sqrt(critical) * (1. / acc + 1. / dec);
     }
-    return angle / top + top / 2 * (1. / acc + 1. / dec);
+    return angle / top + top * (1. / acc + 1. / dec) / 2.;
 }
 
 double Device::angle(double time) const {
     double critical = top * (1. / acc + 1. / dec);
-    if (critical >= time) {
-        return std::min(acc * dec * time * time / (acc + dec), 180.);
+    if (critical > time) {
+        double res = acc * dec * time * time / (acc + dec) / 2.;
+        return res;
     }
-    return std::min(top * (time - critical / 2.), 180.);
+    double res =  top * (time - critical / 2.);
+    return res;
 }
 
 
-#include <iostream>
 using namespace std;
+
+int TEST_NUM = 1;
+
+void expect_eq(double ac, double ex) {
+    const double abs = std::abs(ac - ex);
+    const double sum = std::abs(ex + ac);
+    const double rel = sum > 0 ? std::abs(ac - ex) / std::abs(ex + ac) : abs;
+    std::cout << "TEST #" << TEST_NUM++ << "\t";
+    if (rel < 1e-12) {
+        std::cout << "PASSED";
+    }
+    else {
+        std::cout << " | rel: " << rel << ", abs: " << abs << " " << ac << " " << ex << "\n";
+        std::cout << "NOT PASSED";
+    }
+    std::cout << std::endl;
+    
+
+}
+
+void test_case_invariant(const Device& d, double time, double angle) {
+    expect_eq(d.time(d.angle(time)), time);
+    expect_eq(d.angle(d.time(angle)), angle);
+}
+
+void test_case_sym() {
+    Device d(10, 10, 10);
+    expect_eq(d.angle(0.0), 0.0);
+    expect_eq(d.angle(1.0), 2.5);
+    expect_eq(d.angle(2.0), 10.0);
+    expect_eq(d.angle(3.0), 20.0);
+    expect_eq(d.angle(4.0), 30.0);
+}
+
+void test_case_asym() {
+    Device d1(1, 2, 10);
+    expect_eq(d1.angle(0.0), 0.0);
+    expect_eq(d1.angle(3.0), 3.0);
+
+    Device d2(1, 0.5, 10);
+    expect_eq(d2.angle(3.0), 1.5);
+}
+
+void run_tests() {
+    Device d(10, 10, 10);
+    const int n = 20;
+    const double angle_step = 180.0 / n;
+    const double time_step = 3600.0 / n;
+    test_case_invariant(d, time_step, angle_step);
+    for (int i = 0; i < n; ++i) {
+        test_case_invariant(d, i * time_step, i * angle_step);
+    }
+    test_case_sym();
+    test_case_asym();
+}
 
 int main()
 {
     Device d(5, 3, 20);
     cout << d.time(30) << std::endl;
     cout << d.angle(5) << std::endl;
+    run_tests();
     return 0;
 }
 
