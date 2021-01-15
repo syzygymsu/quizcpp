bbcdf92f2605d43c74271a5fdc173abe3c731d7c
a.savanovich <asavanster@gmail.com>
2021-01-15 06:58:59 +0300

    rewrite hirsh test

--- hirsch_index/main.cpp
+++ hirsch_index/main.cpp
@@ -23,17 +23,33 @@ int hirsh(std::vector<int>& v) {
 }
 
 #include <iostream>
-#include <cassert>
 
 int main()
 {
-	std::vector<int> v{ 5, 6, 4, 3, 2, 6, 7, 9, 3, 0, 0, 1, 2, 2, 3 };
-	assert(hirsh(v) == 5);
-	std::vector<int> v1{ 100 };
-	assert(hirsh(v1) == 1);
-	std::vector<int> v2{ 2, 2, 2, 2, 2 };
-	assert(hirsh(v2) == 2);
-	std::vector<int> v3{ 0, 1, 2, 5, 4, 3 };
-	assert(hirsh(v3) == 3);
-	// std::cout << hirsh(v1) << std::endl;
+	std::vector<std::pair<std::vector<int>, int>> tests = {
+		{{ 5, 6, 4, 3, 2, 6, 7, 9, 3, 0, 0, 1, 2, 2, 3 }, 5},
+		{{ 100 }, 1},
+		{{ 2, 2, 2, 2, 2 }, 2 },
+		{{ 0, 1, 2, 5, 4, 3 }, 3 },
+		{{ 0, 0, 0, 0, 0, 0 }, 0 },
+		{{ 1, 2, 3 }, 2 },
+		{{ 1, 1, 3 }, 1 },
+	};
+	for (int i = 0; i < tests.size(); ++i) {
+		int expected = tests[i].second;
+		auto data = tests[i].first;
+		int result = hirsh(data);
+		bool has_error = false;
+		if (result != expected) {
+			const auto& initial_data = tests[i].first;
+			std::cerr << "Error in test " << i << " expected " << expected << " but result " << result << " on (";
+			for (auto s : initial_data) std::cerr << s << " ";
+			std::cerr << "\b)" << std::endl;
+			has_error = true;
+		}
+		if (has_error) {
+			return 1;
+		}
+	}
+	return 0;
 }
