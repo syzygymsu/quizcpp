8fbafa4db568b784ba096f997cc8749d5c78c2e4
a.savanovich <asavanster@gmail.com>
2021-01-27 11:50:19 +0300

    add more hirsh tests

--- hirsch_index/main.cpp
+++ hirsch_index/main.cpp
@@ -4,22 +4,23 @@
 int hirsh(std::vector<int>& v) {
 	auto begin = v.begin();
 	auto end = v.end();
-	auto mid = v.begin() + v.size() / 2;
-	while (end - begin > 1) {
+	while (true) {
+		auto mid = begin + (end - begin) / 2;
+		if (end - begin <= 0) {
+			return v.end() - mid;
+		}
 		std::nth_element(begin, mid, end);
 		int mid_el = *mid;
-		if ((v.end() - mid == mid_el) || (v.end() - mid - 1 == mid_el)) {
+		if ((v.end() - mid == mid_el)) { // || (v.end() - mid - 1 == mid_el)) {
 			return mid_el;
 		}
 		if (v.end() - mid < mid_el) {
-			end = mid - 1;
+			end = mid;
 		}
 		else {
 			begin = mid + 1;
 		}
-		mid = begin + (end - begin) / 2;
 	}
-	return v.end() - mid;
 }
 
 #include <iostream>
@@ -27,19 +28,36 @@ int hirsh(std::vector<int>& v) {
 int main()
 {
 	std::vector<std::pair<std::vector<int>, int>> tests = {
+		{{}, 0 },
+		{{ 0 }, 0 },
+		{{ 0, 0}, 0 },
+		{{ 0, 0, 0}, 0 },
+		{{ 0, 1}, 1 },
+		{{ 1, 2}, 1 },
+		{{ 0, 1, 0}, 1 },
+		{{ 2, 1}, 1 },
+		{{ 1, 2}, 1 },
+		{{ 0, 100}, 1 },
+		{{ 1, 100}, 1 },
+		{{ 100, 1}, 1 },
+		{{ 100, 3}, 2 },
 		{{ 5, 6, 4, 3, 2, 6, 7, 9, 3, 0, 0, 1, 2, 2, 3 }, 5},
-		{{ 100 }, 1},
 		{{ 2, 2, 2, 2, 2 }, 2 },
 		{{ 0, 1, 2, 5, 4, 3 }, 3 },
 		{{ 0, 0, 0, 0, 0, 0 }, 0 },
 		{{ 1, 2, 3 }, 2 },
 		{{ 1, 1, 3 }, 1 },
+		{{ 1, 3}, 1 },
+		{{ 2, 1}, 1 },
+		{{ 2, 2}, 2 },
+		{{ 5, 3}, 2 },
 	};
+
+	bool has_error = false;
 	for (int i = 0; i < tests.size(); ++i) {
 		int expected = tests[i].second;
 		auto data = tests[i].first;
 		int result = hirsh(data);
-		bool has_error = false;
 		if (result != expected) {
 			const auto& initial_data = tests[i].first;
 			std::cerr << "Error in test " << i << " expected " << expected << " but result " << result << " on (";
@@ -47,9 +65,10 @@ int main()
 			std::cerr << "\b)" << std::endl;
 			has_error = true;
 		}
-		if (has_error) {
-			return 1;
-		}
 	}
+	if (has_error) {
+		return 1;
+	}
+
 	return 0;
 }
