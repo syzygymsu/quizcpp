81880f3883e2eed3e6f61eb967a2b94d4cd0fea5
a.savanovich <asavanster@gmail.com>
2021-01-30 18:10:45 +0300

    add warnings flags to cmake

--- dynamic_array/main.cpp
+++ dynamic_array/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <array>
 
 int main()
 {
+	const int N = 5;
+	int Q[N];
 	std::cout << "Enter a positive integer: ";
 	int length;
 	std::cin >> length;
 
 	int *array = new int[length]; // use array new.  Note that length does not need to be constant!
 
+	std::array<int, N> A;
+
 	std::cout << "I just allocated an array of integers of length " << length << '\n';
 
 	array[0] = 5; // set element 0 to value 5
+
+	A[0] = Q[0];
 
 	delete[] array; // use array delete to deallocate array
 
