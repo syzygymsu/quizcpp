658ee581952f200a50c5240a70454add650d6f34
a.savanovich <asavanster@gmail.com>
2021-02-09 14:41:46 +0300

    template

---
+++ interview2021/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    cout << min(2, 5) << endl;
+    assert(true);
+    return 0;
+}
