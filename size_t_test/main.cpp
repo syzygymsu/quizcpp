b883fa27bd3e796bd4590c4716c5cf4c978b59d5
a.savanovich <asavanster@gmail.com>
2021-12-25 22:34:38 +0300

    test max int

--- size_t_test/main.cpp
+++ size_t_test/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 #include <cassert>
 
@@ -7,5 +8,11 @@ int main()
 	std::vector<int> a;
 	int size = a.size() - 1;
 	assert(size == -1);
+	std::cout << std::numeric_limits<int>::max() << std::endl;
+	std::cout << std::numeric_limits<size_t>::max() << std::endl;
+	size_t big = 2147483648;
+	int test_int = big;
+	assert(test_int < 0);
+	std::cout << test_int << std::endl;
 	return 0;
 }
