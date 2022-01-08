b7ad2b9bce3b5637ac31a622cefa2bd8ec679ab1
a.savanovich <asavanster@gmail.com>
2022-01-08 22:52:40 +0300

    improve reading

--- ya/ya_diag/B/main.cpp
+++ ya/ya_diag/B/main.cpp
@@ -2,256 +2,205 @@
 #include <string>
 #include <vector>
 #include <ctime>
-#include <locale>
-#include <sstream>
+// #include <locale>
 #include <iomanip>
 
-struct Period
-{
-    std::tm date1;
-    std::tm date2;
-};
+namespace {
 
-typedef std::vector<Period> vecPeriods;
+    static const char* format = "%Y-%m-%d";
 
-static void ReturnError()
-{
-    std::cout << "0\n";
-    std::cout << "\n";
-}
-
-static bool StartDateLessThanEnd(std::tm startDate, std::tm endDate)
-{
-    std::time_t start = std::mktime(&startDate);
-    std::time_t end = std::mktime(&endDate);
-    double aux = std::difftime(end, start);
-    if (aux > 0)
+    struct Period
     {
-        return true;
-    }
-    return false;
-}
+        std::tm startDate = {0};
+        std::tm endDate = {0};
+    };
 
-static void CheckIfEndOfPeriodCorrect(Period& temp_pair, std::tm endDate)
-{
-    std::time_t start = std::mktime(&temp_pair.date2);
-    std::time_t end = std::mktime(&endDate);
-    double aux = std::difftime(start, end);
-    if (aux >= 0)
+    typedef std::vector<Period> vecPeriods;
+
+    bool StartDateLessThanEnd(std::tm startDate, std::tm endDate)
     {
-        temp_pair.date2 = endDate;
-    }
-}
-
-static void Normalize(std::tm& curDate)
-{
-    time_t next = mktime(&curDate);
-}
-
-static std::tm AddDays(std::tm curDate, int days)
-{
-    curDate.tm_mday += days;
-    time_t next = mktime(&curDate);
-    return curDate;
-}
-
-static int GetDaysInMonth(const std::tm curDate)
-{
-    std::tm date = curDate;
-    int i = 0;
-    for (i = 29; i <= 31; i++)
-    {
-        date.tm_mday = i;
-        mktime(&date);
-        if (date.tm_mon != curDate.tm_mon)
+        std::time_t start = std::mktime(&startDate);
+        std::time_t end = std::mktime(&endDate);
+        double aux = std::difftime(end, start);
+        if (aux > 0)
         {
-            break;
+            return true;
+        }
+        return false;
+    }
+
+    void print(const Period& period) {
+        std::cout << std::put_time(&period.startDate, format);
+        std::cout << ' ';
+        std::cout << std::put_time(&period.endDate, format);
+        std::cout << '\n';
+    }
+
+    void CheckIfEndOfPeriodCorrect(Period& temp_pair, std::tm endDate)
+    {
+        std::time_t start = std::mktime(&temp_pair.endDate);
+        std::time_t end = std::mktime(&endDate);
+        double aux = std::difftime(start, end);
+        if (aux >= 0)
+        {
+            temp_pair.endDate = endDate;
         }
     }
-    return i - 1;    
-}
 
-static std::tm MONTHEndDate(std::tm curDate)
-{
-    curDate.tm_mday = GetDaysInMonth(curDate);
-    return curDate;
-}
-
-static std::tm WEEKEndDate(std::tm curDate)
-{
-    int DaysTillSunday = 7 - (int)curDate.tm_wday;
-    if (DaysTillSunday == 7)
+    void Normalize(std::tm& curDate)
     {
-        DaysTillSunday = 0;
+        time_t next = mktime(&curDate);
     }
-    
-    std::tm date = AddDays(curDate, DaysTillSunday);
-    return date;
-}
 
-static std::tm REVIEWEndDate(std::tm curDate)
-{
-    std::tm date2 = curDate;
-    if (curDate.tm_mon >= 3 && curDate.tm_mon <= 8)
-    {//summer time
-        date2.tm_mon = 8;//September
-        date2.tm_mday = 30;
-    }
-    else
-    { //Winter time
-        date2.tm_year = curDate.tm_mon <= 11 ? curDate.tm_year + 1 : curDate.tm_year;
-        date2.tm_mon = 2;//March
-        date2.tm_mday = 31;
-    }
-    Normalize(date2);
-    return date2;
-}
-
-static std::tm YEAREndDate(std::tm curDate)
-{
-    //YEAR — год c 1 января по 31 декабря.
-    std::tm date = curDate;
-    date.tm_mon = 11;//Dec
-    date.tm_mday = 31;
-    return date;
-}
-
-static std::tm QUARTEREndDate(std::tm curDate)
-{
-    //интервалы в три месяца: январь — март, апрель — июнь, июль — сентябрь, октябрь — декабрь.
-    //Спионерил в интернете
-    int quarterNumber = (curDate.tm_mon) / 3 + 1;
-    std::tm firstDayOfQuarter = curDate;
-    firstDayOfQuarter.tm_mon = (quarterNumber - 1) * 3;
-    firstDayOfQuarter.tm_mday = 1;
-    Normalize(firstDayOfQuarter);
-
-    auto lastDayOfQuarter = firstDayOfQuarter;
-    lastDayOfQuarter.tm_mon += 3;
-    Normalize(lastDayOfQuarter);
-    lastDayOfQuarter.tm_mday -= 1;
-    Normalize(lastDayOfQuarter);
-    
-    return lastDayOfQuarter;
-}
-
-typedef std::tm (*PeriodProcessingDelegate)(std::tm curDate);
-
-static void ProcessPeriod(vecPeriods& listOfPeriods, std::tm startDate, std::tm endDate, PeriodProcessingDelegate func)
-{
-
-    std::tm curDate;
-
-    curDate = startDate;
-
-    Period temp_pair;
-    temp_pair.date1 = curDate;
-    temp_pair.date2 = func(curDate);
-
-    CheckIfEndOfPeriodCorrect(temp_pair, endDate);
-
-    listOfPeriods.push_back(temp_pair);
-    curDate = temp_pair.date2;
-
-    while (StartDateLessThanEnd(curDate, endDate))
+    std::tm AddDays(std::tm curDate, int days)
     {
+        curDate.tm_mday += days;
+        time_t next = mktime(&curDate);
+        return curDate;
+    }
+
+    int GetDaysInMonth(const std::tm& curDate)
+    {
+        std::tm date = curDate;
+        int i = 0;
+        for (i = 29; i <= 31; i++)
+        {
+            date.tm_mday = i;
+            mktime(&date);
+            if (date.tm_mon != curDate.tm_mon)
+            {
+                break;
+            }
+        }
+        return i - 1;
+    }
+
+    std::tm MONTHEndDate(std::tm curDate)
+    {
+        curDate.tm_mday = GetDaysInMonth(curDate);
+        return curDate;
+    }
+
+    std::tm WEEKEndDate(std::tm curDate)
+    {
+        int DaysTillSunday = 7 - (int)curDate.tm_wday;
+        if (DaysTillSunday == 7)
+        {
+            DaysTillSunday = 0;
+        }
+
+        std::tm date = AddDays(curDate, DaysTillSunday);
+        return date;
+    }
+
+    std::tm REVIEWEndDate(std::tm curDate)
+    {
+        std::tm date2 = curDate;
+        if (curDate.tm_mon >= 3 && curDate.tm_mon <= 8)
+        {//summer time
+            date2.tm_mon = 8;//September
+            date2.tm_mday = 30;
+        }
+        else
+        { //Winter time
+            date2.tm_year = curDate.tm_mon <= 11 ? curDate.tm_year + 1 : curDate.tm_year;
+            date2.tm_mon = 2;//March
+            date2.tm_mday = 31;
+        }
+        Normalize(date2);
+        return date2;
+    }
+
+    std::tm YEAREndDate(std::tm curDate)
+    {
+        //YEAR — год c 1 января по 31 декабря.
+        std::tm date = curDate;
+        date.tm_mon = 11;//Dec
+        date.tm_mday = 31;
+        return date;
+    }
+
+    std::tm QUARTEREndDate(std::tm curDate)
+    {
+        //интервалы в три месяца: январь — март, апрель — июнь, июль — сентябрь, октябрь — декабрь.
+        //Спионерил в интернете
+        int quarterNumber = (curDate.tm_mon) / 3 + 1;
+        std::tm firstDayOfQuarter = curDate;
+        firstDayOfQuarter.tm_mon = (quarterNumber - 1) * 3;
+        firstDayOfQuarter.tm_mday = 1;
+        Normalize(firstDayOfQuarter);
+
+        auto lastDayOfQuarter = firstDayOfQuarter;
+        lastDayOfQuarter.tm_mon += 3;
+        Normalize(lastDayOfQuarter);
+        lastDayOfQuarter.tm_mday -= 1;
+        Normalize(lastDayOfQuarter);
+
+        return lastDayOfQuarter;
+    }
+
+    typedef std::tm(*PeriodProcessingDelegate)(std::tm curDate);
+
+    vecPeriods ProcessPeriod(const Period& p, PeriodProcessingDelegate func)
+    {
+        vecPeriods listOfPeriods;
+
+        std::tm curDate = p.startDate;
+
         Period temp_pair;
-        temp_pair.date1 = AddDays(curDate, 1);
-        temp_pair.date2 = func(temp_pair.date1);
+        temp_pair.startDate = curDate;
+        temp_pair.endDate = func(curDate);
+
+        CheckIfEndOfPeriodCorrect(temp_pair, p.endDate);
+
         listOfPeriods.push_back(temp_pair);
-        curDate = temp_pair.date2;
-    }
-    if (listOfPeriods.size() > 1)
-    {
-        //trim last period
-        CheckIfEndOfPeriodCorrect(listOfPeriods.back(), endDate);
-    }
-}
+        curDate = temp_pair.endDate;
 
-static void MainProcess(std::string sPeriodType, std::string sPeriod)
-{
-    size_t pos = 0;
-    std::string arDates[2];
-    std::string sEndDate;
-    
-    {
-        if ((pos = sPeriod.find(' ')) == std::string::npos)
+        while (StartDateLessThanEnd(curDate, p.endDate))
         {
-            ReturnError();
-            return;
+            Period temp_pair;
+            temp_pair.startDate = AddDays(curDate, 1);
+            temp_pair.endDate = func(temp_pair.startDate);
+            listOfPeriods.push_back(temp_pair);
+            curDate = temp_pair.endDate;
         }
-        
-        arDates[0] = sPeriod.substr(0, pos);
-        arDates[1] = sPeriod.substr(pos + 1);
-        
-    }
-
-
-    vecPeriods listOfPeriods;
-
-    //2020-01-10 2020-03-25
-    std::string format = "%Y-%m-%d";
-    std::tm startDate = { 0 };
-    std::tm endDate = { 0 };
-    try
-    {
-        std::istringstream ss(arDates[0]);
-        ss >> std::get_time(&startDate, format.c_str());
-        std::istringstream ss1(arDates[1]);
-        ss1 >> std::get_time(&endDate, format.c_str());
-    }
-    catch (std::exception)
-    {
-        ReturnError();
-        return;
-    }
-
-    Normalize(startDate);
-    Normalize(endDate);
-    if (sPeriodType == "WEEK")
-    {
-        ProcessPeriod(listOfPeriods, startDate, endDate, WEEKEndDate);
-    }
-    if (sPeriodType == "MONTH")
-    {
-        ProcessPeriod(listOfPeriods, startDate, endDate, MONTHEndDate);
-    }
-    if (sPeriodType == "REVIEW")
-    {
-        ProcessPeriod(listOfPeriods, startDate, endDate, REVIEWEndDate);
-    }
-    if (sPeriodType == "YEAR")
-    {
-        ProcessPeriod(listOfPeriods, startDate, endDate, YEAREndDate);
-    }
-    if (sPeriodType == "QUARTER")
-    {
-        ProcessPeriod(listOfPeriods, startDate, endDate, QUARTEREndDate);
-    }
-
-    std::cout << listOfPeriods.size();
-    std::cout << "\n";
-    for (auto i = 0; i < listOfPeriods.size(); i++)
-    {
-        auto d1 = listOfPeriods[i].date1;
-        auto d2 = listOfPeriods[i].date2;
-        std::ostringstream oss;
-        oss << std::put_time(&listOfPeriods[i].date1, format.c_str());
-
-        std::ostringstream oss1;
-        oss1 << std::put_time(&listOfPeriods[i].date2, format.c_str());
-        //format = "yyyy-MM-dd";
-        std::cout << oss.str() << " " << oss1.str() << "\n";
+        if (listOfPeriods.size() > 1)
+        {
+            //trim last period
+            CheckIfEndOfPeriodCorrect(listOfPeriods.back(), p.endDate);
+        }
+        return listOfPeriods;
     }
 }
+
 int main()
 {
-    
+    vecPeriods listOfPeriods;
     std::string sPeriodType;
-    std::string sPeriod;
+    std::cin >> sPeriodType;
 
-    std::getline(std::cin, sPeriodType);
-    std::getline(std::cin, sPeriod);
+    Period p;
+    std::cin >> std::get_time(&p.startDate, format);
+    std::cin >> std::get_time(&p.endDate, format);
+    Normalize(p.startDate);
+    Normalize(p.endDate);
 
-    MainProcess(sPeriodType, sPeriod);
+    if (sPeriodType == "WEEK") {
+        listOfPeriods = ProcessPeriod(p, WEEKEndDate);
+    } else if (sPeriodType == "MONTH") {
+        listOfPeriods = ProcessPeriod(p, MONTHEndDate);
+    } else if (sPeriodType == "REVIEW") {
+        listOfPeriods = ProcessPeriod(p, REVIEWEndDate);
+    } else if (sPeriodType == "YEAR") {
+        listOfPeriods = ProcessPeriod(p, YEAREndDate);
+    } else if (sPeriodType == "QUARTER") {
+        listOfPeriods = ProcessPeriod(p, QUARTEREndDate);
+    }
+
+    std::cout << listOfPeriods.size() << '\n';
+    for (const Period& p : listOfPeriods) {
+        print(p);
+    }
+    std::cout << std::flush;
+
 }
