c52a413bd02c94325457ab54d4db3f49f570c381
a.savanovich <asavanster@gmail.com>
2024-04-01 18:42:39 +0300

    factorization 13ms

--- ya_algo_2020_09/D2/main.cpp
+++ ya_algo_2020_09/D2/main.cpp
@@ -1,7 +1,6 @@
 // https://contest.yandex.ru/contest/19811/problems/D/
 
 #include <iostream>
-#include <cmath>
 
 long long combine(long long& n, long long d) {
     long long tmp = 1;
@@ -14,16 +13,18 @@ long long combine(long long& n, long long d) {
     return tmp * (d - 1);
 }
 
-long long primeFactors(long long& n, long long limit) {
+long long primeFactors(long long& n) {
     long long res = 1;
     if (n % 2 == 0) {
         res *= combine(n, 2);
     }
-
-    for (int i = 3; i <= limit; i = i + 2) {
+    for (long long i = 3; i * i <= n; i = i + 2) {
         if (n % i == 0) {
             res *= combine(n, i);
         }
+    }
+    if (n > 1) {
+        return (n - 1) * res;
     }
     return res;
 }
@@ -34,12 +35,7 @@ long long fast(long long n) {
     if (n == 1) {
         return 2;
     }
-    long long limit = (long long)std::sqrt(n) + 1;
-    long long res = primeFactors(n, limit);
-    if (n < limit) {
-        return res;
-    }
-    return res * (n - 1);
+    return primeFactors(n);
 }
 
 int main() {
